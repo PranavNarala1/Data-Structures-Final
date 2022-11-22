6b3a4930bbc69192e90afb41fb1007fdda7e0893
Eric <70232980+pogman96@users.noreply.github.com>
2022-11-22 16:14:36 -0500

    init

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "neuralnetwork.h"
+
+double* readInput(char* filename, int* size);
 
 int main(int argc, char *argv[]) {
     srand(42);
+
+    FILE *stream = fopen("data/citrus.csv", "r");
+    if (stream == NULL)
+        printf("Error: Could not open file")
+
+    char line[1024];
+    while (fgets(line, 1024, stream)) {
+        char* tmp = strdup(line);
+        printf("%s\n", tmp);
+    }
+
     return 0;
 }
