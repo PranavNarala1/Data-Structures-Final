75005589d3270bba65472868084c96db31d7f529
Eric <70232980+pogman96@users.noreply.github.com>
2022-11-23 05:20:54 +0000

    read data from file into 2 arrays

--- main.c
+++ main.c
@@ -3,19 +3,39 @@
 #include <string.h>
 #include "neuralnetwork.h"
 
-double* readInput(char* filename, int* size);
-
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     srand(42);
 
+    int *inputLabels = calloc(10000, sizeof(int));
+    double **inputData = calloc(10000, sizeof(double *));
+    // todo split into training
+
     FILE *stream = fopen("data/citrus.csv", "r");
+
     if (stream == NULL)
-        printf("Error: Could not open file")
+        printf("Could not open file");
 
     char line[1024];
-    while (fgets(line, 1024, stream)) {
-        char* tmp = strdup(line);
-        printf("%s\n", tmp);
+    size_t counter = 0;
+    while (fgets(line, 1024, stream))
+    {
+        char *tmp = strdup(line);
+        double *tempArr = calloc(5, sizeof(double));
+
+        char *label = strtok(tmp, ",");
+        inputLabels[counter] = strcmp(label, "grapefruit") == 0 ? 1 : 0;
+
+        for (size_t i = 0; i < 5; ++i)
+        {
+            char *tempData = strtok(NULL, ",");
+            char *ptr;
+            tempArr[i] = strtod(tempData, &ptr);
+        }
+
+        inputData[counter] = tempArr;
+
+        ++counter;
     }
 
     return 0;
